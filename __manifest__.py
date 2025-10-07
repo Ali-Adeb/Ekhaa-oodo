68f93e1f27dab47f2433a2a3c21fa569c21d763c
ali <ali.d.humrna@gmail.com>
2025-10-07 12:56:02 +0300

    Initial commit: add my_module

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "My Module",
+    "version": "1.0",
+    "summary": "مثال على موديول Odoo",
+    "description": "موديول تجريبي لشرح CI/CD",
+    "author": "Your Name",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": True,
+}
